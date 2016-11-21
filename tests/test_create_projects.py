b38d6b58471432df3cab8978c3f34cf40e0f16a0
Jeff Loiselle <jeff@loiselles.com>
2016-11-21 16:44:21 -0500

    changed test names, added tests for service accounts

--- tests/test_create_projects.py
+++ tests/test_create_projects.py
@@ -1,9 +1,10 @@
 from __future__ import absolute_import
 
-import mock
 import unittest
+from mock import call, mock
 
 from gcptools.create_projects import create_project
+from gcptools.create_projects import create_service_accounts
 
 # Super useful links about testing
 #
@@ -16,8 +17,8 @@ from gcptools.create_projects import create_project
 class CreateProjectsTest(unittest.TestCase):
 
     @mock.patch('gcptools.create_projects.google')
-    def testCreateProjectWithOrganization(self, mock_google):
-    # def testCreateProject(self):
+    def test_create_project_with_organization(self, mock_google):
+    # def test_create_project_with_organization(self):
         project_id = 1
         settings = {
             'organization': 'Broad',
@@ -27,8 +28,8 @@ class CreateProjectsTest(unittest.TestCase):
         mock_google.create_project.assert_called_with({'projectId': 1, 'name': 1, 'parent': {'type': 'organization', 'id': 'Broad'}})
 
     @mock.patch('gcptools.create_projects.google')
-    def testCreateProjectWithFolder(self, mock_google):
-    # def testCreateProjectWithFolder(self):
+    def test_create_project_with_folder(self, mock_google):
+    # def test_create_project_with_folder(self):
         project_id = 1
         settings = {
             'organization': 'Broad',
@@ -36,6 +37,16 @@ class CreateProjectsTest(unittest.TestCase):
         }
         create_project(project_id, settings)
         mock_google.create_project.assert_called_with({'projectId': 1, 'name': 1, 'parent': {'type': 'organization', 'id': 'Broad'}})
+
+    @mock.patch('gcptools.create_projects.google')
+    def test_create_service_accounts(self, mock_google):
+    # def test_create_service_accounts(self):
+        project_id = 1
+        settings = {
+            'service_accounts': [1,2]
+        }
+        create_service_accounts(project_id, settings)
+        mock_google.create_service_account.assert_has_calls([call(1,1), call(1,2)], any_order=True)
 
 if __name__ == '__main__':
 	unittest.main()

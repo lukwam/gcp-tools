9bfa60690da54ff20bc924d4a00375773624ddef
Lukas Karlsson <karlsson@broadinstitute.org>
2017-01-18 22:31:14 -0500

    Fixed run script.

--- run.sh
+++ run.sh
@@ -4,15 +4,11 @@ DOCKER_SOCKET='/var/run/docker.sock'
 SUDO=
 TTY=
 
-SCRIPT_DIR="$( cd -P "$( dirname "$BASH_SOURCE[0]" )" && pwd )"
-
-pushd $SCRIPT_DIR >/dev/null
-
-if [ "$TERM" != "dumb" ]; then
+if [ "${TERM}" != "dumb" ]; then
         TTY='-it'
 fi
 
-if [ `uname -s` != "Darwin" ]; then
+if [ "$(uname -s)" != "Darwin" ]; then
     if [ ! -w "${DOCKER_SOCKET}" ]; then
         SUDO='sudo'
     fi
@@ -21,10 +17,8 @@ fi
 $SUDO docker run $TTY --rm \
         -e GOOGLE_APPLICATION_CREDENTIALS=/root/.config/gcloud/application_default_credentials.json \
         -e PATH=/usr/src:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
-        -v `pwd`:/usr/src \
+        -v "$(pwd)":/usr/src \
         -v ~/.config:/root/.config \
         -v /etc/localtime:/etc/localtime:ro \
         -w /usr/src \
-        lukwam/gcp-tools:local $*
-
-popd >/dev/null
+        lukwam/gcp-tools "$@"
